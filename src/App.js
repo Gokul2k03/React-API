90b25da26b497bb35e69fe590d35357d65f9936f
Gokul2k03 <2003gokulmohan@gmail.com>
2023-04-01 22:26:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Loader } from 'rsuite';
 
 function App() {
+  const [users, setUsers] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  useEffect(() => {
+    axios.get('https://602e7c2c4410730017c50b9d.mockapi.io/users')
+      .then(res => {
+        setUsers(res.data);
+      });
+  }, []);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container user'>
+        <div class="card text-bg-primary mb-3">
+          <div className='card-header'>
+            Users List
+          </div>
+        </div>
+        {users ? (
+          users.map((user) => {
+            return (
+              <div className='card crd' key={user.name} onClick={() => handleUserClick(user)}>
+                <div class="card-body">
+                  <img className='img' src='{user.avatar}' alt='user-profile' />
+                  <span className='card-title'>{user.profile.firstName}</span>
+                  <span className='card-title'>{user.profile.lastName}</span>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <Loader center content="loading" />
+        )}
+      </div>
+      <div className='container details'>
+      <div class="card details-card">
+          <div className='card-header text-bg-primary mb-3 '>
+            User Details
+          </div>
+      {selectedUser ? (
+          <div class="card-body">
+            <img className='img img-details' src='{user.avatar}' alt='user-profile' />
+            <p className='username'>@{selectedUser.profile.username}</p>
+              <ul class="list-group">
+                Bio
+                <li class="list-group-item list-group-item-secondary rounded">{selectedUser.Bio}</li>
+                Full Name:
+                <li class="list-group-item list-group-item-secondary rounded">
+                  <span>
+                    {selectedUser.profile.firstName}
+                  </span> 
+                  <span>
+                    {selectedUser.profile.lastName}
+                  </span>
+                </li>
+                Job Title:
+                <li class="list-group-item list-group-item-secondary rounded">{selectedUser.jobTitle}</li>
+                E-Mail:
+                <li class="list-group-item list-group-item-secondary rounded">{selectedUser.profile.email}</li>
+              </ul>
+          </div>
+        
+      ) : null}
+      </div>
+      </div>
     </div>
   );
+  
 }
+
 
 export default App;
